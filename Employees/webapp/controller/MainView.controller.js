cfbfbe3cc490c3bb8008cf73d7553de342a8bd5e
sppsergi96 <sergi_gp_@hotmail.com>
2021-06-14 22:39:14 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 sap.ui.define([
 		"sap/ui/core/mvc/Controller"
 	],
@@ -7,9 +8,37 @@ sap.ui.define([
 	function (Controller) {
 		"use strict";
 
+        /*function myCheck() {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                inputEmployee.setDescription("OK");
+            } else {
+                inputEmployee.setDescription("Not OK");
+            }
+        }*/
+
 		return Controller.extend("logaligroup.Employees.controller.MainView", {
+
 			onInit: function () {
 
-			}
+            },
+            
+            //onValidate: myCheck
+            onValidate: function () {
+                var inputEmployee =  this.getView().byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                    //inputEmployee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("inputCountry").setVisible(true);
+                } else {
+                    //inputEmployee.setDescription("Not OK");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("inputCountry").setVisible(false);
+                }
+            }
 		});
 	});
